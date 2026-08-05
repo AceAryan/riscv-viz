c3b9539130e7fe8907233f5cc69f7b6c9a24cea7
AceAryan <acearyan8@gmail.com>
2026-08-06 01:07:48 +0530

    chore: update server to allow Vercel URL

--- backend/server.py
+++ backend/server.py
@@ -10,7 +10,7 @@ app = FastAPI()
 # allow React frontend to call this API
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["https://riscv-viz.vercel.app", "http://localhost:5174"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
